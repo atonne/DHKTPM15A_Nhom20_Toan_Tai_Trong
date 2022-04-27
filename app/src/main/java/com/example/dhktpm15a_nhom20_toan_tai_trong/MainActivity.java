693d679dc6280aaeb37520e63717aa9f0ea326fe
TrongPhan1011 <huutrong2k1@gmail.com>
2022-04-27 17:21:38 +0700

    create project, base

---
+++ app/src/main/java/com/example/dhktpm15a_nhom20_toan_tai_trong/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.dhktpm15a_nhom20_toan_tai_trong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
