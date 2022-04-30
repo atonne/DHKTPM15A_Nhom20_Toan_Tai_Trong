1443f6e08494f1c12382732cfd264ebca8d90b59
letuantai15f <letuantai15f@gmail.com>
2022-04-30 16:31:49 +0700

    tai_chitietnote_2

--- app/src/main/java/com/example/dhktpm15a_nhom20_toan_tai_trong/ChiTietNode.java
+++ app/src/main/java/com/example/dhktpm15a_nhom20_toan_tai_trong/ChiTietNode.java
@@ -8,21 +8,23 @@ import android.view.View;
 import android.widget.TextView;
 
 public class ChiTietNode extends AppCompatActivity {
-    TextView tvquaylai;
+    TextView tvquaylai,tvluu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chi_tiet_node);
+        anhxa();
         tvquaylai.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(ChiTietNode.this,trangchu.class);
-                startActivity(intent);
+//                Intent intent=new Intent(ChiTietNode.this,trangchu.class);
+//                startActivity(intent);
 
             }
         });
     }
     public void anhxa(){
         tvquaylai=findViewById(R.id.tv_quaylai_chitietnode);
+        tvluu=findViewById(R.id.tvluu);
     }
 }
