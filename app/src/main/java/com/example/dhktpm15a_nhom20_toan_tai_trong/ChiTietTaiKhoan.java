ab8f2a622682c5a3eeee180e34fd6816ed3401b4
thanh toan <5442654@gmail.com>
2022-04-30 14:20:40 +0700

    layout chi tiet thong tin va java chi tiet thong tin

---
+++ app/src/main/java/com/example/dhktpm15a_nhom20_toan_tai_trong/ChiTietTaiKhoan.java
@@ -0,0 +1,30 @@
+package com.example.dhktpm15a_nhom20_toan_tai_trong;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChiTietTaiKhoan extends AppCompatActivity  implements View.OnClickListener{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chitiettaikhoan);
+        TextView tvQuayLai = findViewById(R.id.tv_quaylai);
+        tvQuayLai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              //  Intent intent=new Intent(ChiTietTaiKhoan.this,trang.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
